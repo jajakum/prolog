b6a57759bdacd87ac647005bb3c49f1c3d340270
artyom <jajakumm@gmail.com>
2021-02-06 21:53:33 +0300

    Zadanie 2.pl
    Задание 2.

--- family1.pl
+++ family1.pl
@@ -18,5 +18,38 @@ woman(tanya).
 woman(natasha).
 woman(masha).
 
+parent(ashot,dmitry).
+parent(ashot,gulnara).
+parent(ashot,daniel).
+parent(ashot,natasha).
+
+parent(marina,dmitry).
+parent(marina,gulnara).
+parent(marina,daniel).
+parent(marina,natasha).
+
+parent(vlad,vladimir).
+parent(vlad,daniel).
+parent(lera,vladimir).
+parent(lera,daniel).
+
+parent(david,nikita).
+parent(david,nastya).
+parent(dasha,nikita).
+parent(dasha,nastya).
+
+parent(dmitry,masha).
+parent(dmitry,tanya).
+parent(natasha,masha).
+parent(natasha,tanya).
+
+parent(artyom,dasha).
+parent(artyom,artur).
+parent(diana,dasha).
+parent(diana,artur).
+
 women(X):-woman(X), write(X), nl, fail.
 men(X):-man(X), write(X), nl, fail.
+
+child(X,Y):-parent(Y,X).
+children(X):-parent(X,Y), write(Y), nl, fail.
