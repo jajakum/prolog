50e0cf1a71e058dafc9c5428c51449121ba49394
artyom <jajakumm@gmail.com>
2021-02-15 00:26:19 +0300

    Akinator
    lab 2 task 2

---
+++ akinator.pl
@@ -0,0 +1,349 @@
+footballer(messi, 1).
+footballer(ronaldo, 1).
+footballer(lewandowsky, 1).
+footballer(aguero, 1).
+footballer(belenov, 1).
+footballer(yashin, 1).
+footballer(shapi, 1).
+footballer(pogba, 1).
+footballer(vandijk, 1).
+footballer(raniery, 0).
+footballer(guardyola, 0).
+
+coach(messi, 0).
+coach(ronaldo, 0).
+coach(lewandowsky, 0).
+coach(aguero, 0).
+coach(belenov, 0).
+coach(yashin, 0).
+coach(shapi, 0).
+coach(pogba, 0).
+coach(raniery, 1).
+coach(guardyola, 1).
+coach(vandijk, 0).
+
+leftfoot(messi, 1).
+leftfoot(ronaldo, 0).
+leftfoot(lewandowsky, 0).
+leftfoot(aguero, 0).
+leftfoot(belenov, 0).
+leftfoot(yashin, 0).
+leftfoot(shapi, 1).
+leftfoot(pogba, 0).
+leftfoot(raniery, 0).
+leftfoot(guardyola, 0).
+leftfoot(vandijk, 0).
+
+rightfoot(messi, 0).
+rightfoot(ronaldo, 1).
+rightfoot(lewandowsky, 1).
+rightfoot(aguero, 1).
+rightfoot(belenov, 1).
+rightfoot(yashin, 1).
+rightfoot(shapi, 0).
+rightfoot(pogba, 1).
+rightfoot(raniery, 1).
+rightfoot(guardyola, 1).
+rightfoot(vandijk, 1).
+
+goldenball(messi, 1).
+goldenball(ronaldo, 1).
+goldenball(lewandowsky, 0).
+goldenball(aguero, 0).
+goldenball(belenov, 0).
+goldenball(yashin, 1).
+goldenball(shapi, 0).
+goldenball(pogba, 0).
+goldenball(raniery, 0).
+goldenball(guardyola, 0).
+goldenball(vandijk, 0).
+
+goldenfoot(messi, 1).
+goldenfoot(ronaldo, 1).
+goldenfoot(lewandowsky, 1).
+goldenfoot(aguero, 0).
+goldenfoot(belenov, 0).
+goldenfoot(yashin, 0).
+goldenfoot(shapi, 0).
+goldenfoot(pogba, 0).
+goldenfoot(raniery, 0).
+goldenfoot(guardyola, 0).
+goldenfoot(vandijk, 0).
+
+defender(messi, 0).
+defender(ronaldo, 0).
+defender(lewandowsky, 0).
+defender(aguero, 0).
+defender(belenov, 0).
+defender(yashin, 0).
+defender(shapi, 0).
+defender(pogba, 0).
+defender(raniery, 0).
+defender(guardyola, 0).
+defender(vandijk, 1).
+
+midfielder(messi, 1).
+midfielder(ronaldo, 0).
+midfielder(lewandowsky, 0).
+midfielder(aguero, 0).
+midfielder(belenov, 0).
+midfielder(yashin, 0).
+midfielder(shapi, 0).
+midfielder(pogba, 1).
+midfielder(raniery, 1).
+midfielder(guardyola, 1).
+midfielder(vandijk, 0).
+
+striker(messi, 0).
+striker(ronaldo, 1).
+striker(lewandowsky, 1).
+striker(aguero, 1).
+striker(belenov, 0).
+striker(yashin, 0).
+striker(shapi, 1).
+striker(pogba, 0).
+striker(raniery, 0).
+striker(guardyola, 0).
+striker(vandijk, 0).
+
+goalkeeper(messi, 0).
+goalkeeper(ronaldo, 0).
+goalkeeper(lewandowsky, 0).
+goalkeeper(aguero, 0).
+goalkeeper(belenov, 1).
+goalkeeper(yashin, 1).
+goalkeeper(shapi, 0).
+goalkeeper(pogba, 0).
+goalkeeper(raniery, 0).
+goalkeeper(guardyola, 0).
+goalkeeper(vandijk, 0).
+
+ex-footballer(messi, 0).
+ex-footballer(ronaldo, 0).
+ex-footballer(lewandowsky, 0).
+ex-footballer(aguero, 0).
+ex-footballer(belenov, 1).
+ex-footballer(yashin, 1).
+ex-footballer(shapi, 0).
+ex-footballer(pogba, 0).
+ex-footballer(raniery, 1).
+ex-footballer(guardyola, 1).
+ex-footballer(vandijk, 0).
+
+barcelona(messi, 1).
+barcelona(ronaldo, 0).
+barcelona(lewandowsky, 0).
+barcelona(aguero, 0).
+barcelona(belenov, 0).
+barcelona(yashin, 0).
+barcelona(shapi, 0).
+barcelona(pogba, 0).
+barcelona(raniery, 0).
+barcelona(guardyola, 1).
+barcelona(vandijk, 0).
+
+realmadrid(messi, 0).
+realmadrid(ronaldo, 1).
+realmadrid(lewandowsky, 0).
+realmadrid(aguero, 0).
+realmadrid(yashin, 0).
+realmadrid(shapi, 0).
+realmadrid(pogba, 0).
+realmadrid(raniery, 0).
+realmadrid(guardyola, 0).
+realmadrid(vandijk, 0).
+realmadrid(belenov, 0).
+
+bayernmunich(messi, 0).
+bayernmunich(ronaldo, 0).
+bayernmunich(lewandowsky, 1).
+bayernmunich(aguero, 0).
+bayernmunich(yashin, 0).
+bayernmunich(pogba, 0).
+bayernmunich(raniery, 0).
+bayernmunich(guardyola, 0).
+bayernmunich(vandijk, 0).
+bayernmunich(belenov, 0).
+bayernmunich(shapi, 0).
+
+mancity(messi, 0).
+mancity(lewandowsky, 0).
+mancity(ronaldo, 0)
+mancity(yashin, 0).
+mancity(pogba, 0).
+mancity(raniery, 0).
+mancity(guardyola, 0).
+mancity(aguero, 1).
+mancity(vandijk, 0).
+mancity(belenov, 0).
+mancity(shapi, 0).
+
+kuban(messi, 0).
+kuban(lewandowsky, 0).
+kuban(ronaldo, 0).
+kuban(yashin, 0).
+kuban(pogba, 0).
+kuban(raniery, 0).
+kuban(guardyola, 0).
+kuban(aguero, 0).
+kuban(vandijk, 0).
+kuban(belenov, 1).
+kuban(shapi, 0).
+
+dynamo(messi, 0).
+dynamo(lewandowsky, 0).
+dynamo(ronaldo, 0).
+dynamo(yashin, 1).
+dynamo(pogba, 0).
+dynamo(raniery, 0).
+dynamo(guardyola, 0).
+dynamo(aguero, 0).
+dynamo(vandijk, 0).
+dynamo(belenov, 0).
+dynamo(shapi, 0).
+
+
+krasnodar(messi, 0).
+krasnodar(lewandowsky, 0).
+krasnodar(ronaldo, 0).
+krasnodar(yashin, 0).
+krasnodar(pogba, 0).
+krasnodar(raniery, 0).
+krasnodar(guardyola, 0).
+krasnodar(aguero, 0).
+krasnodar(vandijk, 0).
+krasnodar(belenov, 0).
+krasnodar(shapi, 1).
+
+manunited(messi, 0).
+manunited(lewandowsky, 0).
+manunited(ronaldo, 0).
+manunited(yashin, 0).
+manunited(pogba, 1).
+manunited(raniery, 0).
+manunited(guardyola, 0).
+manunited(aguero, 0).
+manunited(vandijk, 0).
+manunited(belenov, 0).
+manunited(shapi, 0).
+
+leicester(messi, 0).
+leicester(lewandowsky, 0).
+leicester(ronaldo, 0).
+leicester(yashin, 0).
+leicester(pogba, 0).
+leicester(raniery, 1).
+leicester(guardyola, 0).
+leicester(aguero, 0).
+leicester(vandijk, 0).
+leicester(belenov, 0).
+leicester(shapi, 0).
+
+question1(X1):-	write("Is it acting football player?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X1).
+
+question2(X2):-	write("Is it coach?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X2).
+
+question3(X3):-	write("Is his working leg left?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X3).
+
+question4(X4):-write("Is his working leg right?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X4).
+
+question5(X5):-	write("He got a golden ball?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X5).
+
+question6(X6):-	write("He got a golden foot?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X6).
+
+
+question7(X7):-	write("He deffender?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X7).
+
+question8(X8):-	write("He midfielder?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X8).
+
+question9(X9):-	write("He striker?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X9).
+
+question10(X10):-write("He goalkeeper?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X10).
+
+question11(X11):-write("He ex-footballer?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X11).
+
+question12(X12):-write("He was at the football club Barcelona?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X12).
+
+question13(X13):-write("He was at the football club Real Madrid?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X13).
+
+question14(X14):-write("He was at the football club bayernmunich?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X14).
+
+question15(X15):-write("He was at the football club mancity?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X15).
+
+question16(X16):-write("He was at the football club kuban?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X16).
+
+question17(X17):-write("He was at the football club dynamo?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X17).
+
+question18(X18):-write("He was at the football club krasnodar?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X18).
+
+question19(X19):-write("He was at the football club manunited?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X19).
+
+question20(X20):-write("He was at the football club leicester?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X20).
+
+pr:-	question1(X1),question2(X2),question3(X3),question4(X4),question5(X5),question6(X6),question7(X7),question8(X8),question9(X9),
+question10(X10), question11(X11), question12(X12), question13(X13), question14(X14), question15(X15),
+question16(X16), question17(X17), question18(X18), question19(X19), question20(X20),
+footballer(X,X1), coach(X,X2), leftfoot(X,X3), rightfoot(X,X4), goldenball(X,X5), goldenfoot(X,X6),
+defender(X,X7), midfielder(X,X8), striker(X,X9), goalkeeper(X,X10), ex-footballer(X,X11), barcelona(X,X12),
+realmadrid(X,X13), bayernmunich(X,X14), mancity(X,X15), kuban(X,X16), dynamo(X,X17), krasnodar(X,X18),
+manunited(X,X19), leicester(X,X20), write(X).
