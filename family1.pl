6e8b8ed12d761f7e76800b3289e7431180314b03
artyom <jajakumm@gmail.com>
2021-02-14 21:34:14 +0300

    Update family1.pl
    added task 11

--- family1.pl
+++ family1.pl
@@ -77,3 +77,5 @@ grand_pas(X):-parent(Y,X), parent(Z,Y), man(Z), write(Z), nl, fail.
 grand_so(X,Y):-parent(Z,X), parent(Y,Z), man(X), man(Y).
 grand_sons(X):-parent(X,Y), parent(Y,Z), man(Z), write(Z), nl, fail.
 grand_pa_and_son(X,Y):-(parent(X,Z),parent(Z,Y),man(X),man(Y))|(parent(Z,X),parent(Y,Z),man(X),man(Y)).
+grand_ma_and_son(X,Y):-grand_ma(X,Y),man(Y).
+grand_ma_and_son(X,Y):-grand_ma(Y,X),man(X).
