816501a8c6d2d23e6839012c44186a4c2ae7a167
artyom <jajakumm@gmail.com>
2021-02-28 22:31:56 +0300

    Update lab3.pl
    task 11 end

--- lab3.pl
+++ lab3.pl
@@ -39,3 +39,7 @@ del2(_,Y):-Y is 0,!.
 
 kolvo_up(0,0):-!.
 kolvo_up(N,X):-N1 is N div 10, kolvo_up(N1,X1), del2(N,Y), X is X1+Y,!.
+
+kolvo_down(N,X):-kolvo_down(N,0,X).
+kolvo_down(0,X,X):-!.
+kolvo_down(A,S,X):- Ad is A div 10,del2(A,Y),S1 is S+Y, kolvo_down(Ad,S1,X),!.
