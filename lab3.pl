9bde72c593474c0d9a8e0723403d510088d3979f
artyom <jajakumm@gmail.com>
2021-02-28 20:45:29 +0300

    Update lab3.pl
    task 5

--- lab3.pl
+++ lab3.pl
@@ -11,3 +11,7 @@ fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
 fact1(N,X):-fact1(0,1,N,X).
 fact1(N,X,N,X):-!.
 fact1(I,F,N,X):-I1 is I+1,F1 is F*I1,fact1(I1,F1,N,X).
+
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):-N1 is N-1,N2 is N-2, fib(N1,X1),fib(N2,X2), X is X1+X2.
