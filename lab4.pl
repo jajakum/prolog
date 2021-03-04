e77336891ed85230318e9c1c316f224eb9358cf0
artyom <jajakumm@gmail.com>
2021-03-04 23:41:39 +0300

    Update lab4.pl
    task 2

--- lab4.pl
+++ lab4.pl
@@ -3,3 +3,7 @@ read_list(N,[Head|Tail]):-read(Head),N1 is N-1,read_list(N1,Tail).
 
 write_list([]):-!.
 write_list([Head|Tail]):-write(Head),nl,write_list(Tail).
+
+sum_lis_down(List,Sum):-sum_list_down(List,0,Sum).
+sum_list_down([],Sum,Sum):-!.
+sum_list_down([Head|Tail],S,Sum):-S1 is S+Head,sum_list_down(Tail,S1,Sum).
