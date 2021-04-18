4977e0603db20295a6292c5c962e32aeba5f0515
artyom <jajakumm@gmail.com>
2021-04-18 21:55:10 +0300

    Update lab7.pl

--- lab7/lab7.pl
+++ lab7/lab7.pl
@@ -1,2 +1,19 @@
-%Задание 1 Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
-pr(X):-read_str(List,X) , write_str(List), Z = ",", write(Z),  write_str(List), write(Z),  write_str(List).
+%Задание 1
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+write_list_str([]):-!.
+write_list_str([H|Tail]):-write_str(H),write(" "),write_list_str(Tail).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+append1([],List,List):-!.
+append1([H1|T1],List,[H1|T3]):-append1(T1,List,T3).
+
+pr(X):-read_str(List,X) , write_str(List), Z = ",", write(Z),  write_str(List),
+    write(Z),  write_str(List).
