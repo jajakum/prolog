5488f59250f25b0d78f90875467024210fc095cf
artyom <jajakumm@gmail.com>
2021-04-18 21:56:31 +0300

    Update lab7.pl
    task 2

--- lab7/lab7.pl
+++ lab7/lab7.pl
@@ -17,3 +17,9 @@ append1([H1|T1],List,[H1|T3]):-append1(T1,List,T3).
 
 pr(X):-read_str(List,X) , write_str(List), Z = ",", write(Z),  write_str(List),
     write(Z),  write_str(List).
+
+%Задание 2
+read_str1([H|T],N):-r_str1(H,T,N,0).
+r_str1(_,[],I,N):-I is N+1,!.
+r_str1(32,[H|T],I,N):-I1 is N+1, r_str1(H,T , I, I1),!.
+r_str1(_,[H|T],I,N):-r_str1(H, T, I, N).
