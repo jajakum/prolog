a3f7f206809d05a0216bc044dde81012f2327fbf
artyom <jajakumm@gmail.com>
2021-04-24 00:35:59 +0300

    Create lab9.pl
    task 1

---
+++ lab 9/lab9.pl
@@ -0,0 +1,20 @@
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+read_list_str(List):-read_str(A,N,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+
+%1.1
+build_all_razm_p:-
+		read_str(A,_),read(K),b_a_rp(A,K,[]).
+
+b_a_rp(A,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_rp(A,N,Perm):-in_list(A,El),N1 is N-1,b_a_rp(A,N1,[El|Perm]).
